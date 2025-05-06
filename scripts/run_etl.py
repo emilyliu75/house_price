dd426f56dc13021521d6bab25c33170d1557665c
Xiangnan Liu <150020746+emilyliu75@users.noreply.github.com>
2025-05-06 15:46:46 +0100

    initial set up

---
+++ scripts/run_etl.py
@@ -0,0 +1,23 @@
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from config.env_config import setup_env
+
+
+def main():
+    run_env_setup()
+
+    print(
+        f"ETL pipeline run successfully in "
+        f'{os.getenv("ENV", "error")} environment!'
+    )
+
+
+def run_env_setup():
+    print("Setting up environment...")
+    setup_env(sys.argv)
+    print("Environment setup complete.")
+
+
+if __name__ == "__main__":
+    main()
