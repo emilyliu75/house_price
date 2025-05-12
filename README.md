8af7f5b34963f49658f04a4912d1a1b10a854012
Xiangnan Liu <150020746+emilyliu75@users.noreply.github.com>
2025-05-12 12:09:08 +0100

    Home page renamed and Outward_Code renames

--- README.md
+++ README.md
@@ -16,6 +16,10 @@ ENV=test pytest
 
 
 # user stories:
-As a London first-time buyer, I want to filter the dashboard by borough, property-type and new-build status so that I can see typical prices for the kind of flat I can actually afford.
+As a London first-time buyer, I want to filter the dashboard by borough, property-type so that I can see typical prices for the kind of property I can actually afford.
 
 As a data analyst, I want the ETL pipeline to rerun automatically each month and append only new Land-Registry rows, so that the dashboard always stays current without manual work.
+
+interesting discovery:
+there's only one record for outward code N2, which seems a bit weird
+It turned out that's from another borough
--- streamlit_app/pages/Outward_code.py
+++ streamlit_app/pages/Outward_code.py
@@ -19,6 +19,15 @@ def get_heatmap_data() -> pd.DataFrame:
     """)
     return pd.read_sql(sql, engine)
 
+@st.cache_data(ttl=3600)
+def get_all_data() -> pd.DataFrame:
+    sql = text("""
+        SELECT
+          date, postcode, price, property_type,address
+        FROM clean_house_prices
+    """)
+    return pd.read_sql(sql, engine)
+
 def render():
     df = get_heatmap_data()
     st.subheader("Average sale price by outward code (last 5 years)")
@@ -32,6 +41,23 @@ def render():
            )
     )
     st.altair_chart(chart, use_container_width=True)
+
+    # search by postcode
+    st.subheader("Search by postcode")
+    # Input field
+    user_postcode = st.text_input("Enter a full or partial postcode (e.g., SE10 or W1A 1AA):").strip().upper()
+
+    # Filter results
+    if user_postcode:
+        df_all = get_all_data()
+
+        filtered_df = df_all[df_all['postcode'].str.startswith(user_postcode)]
+
+        if not filtered_df.empty:
+            st.success(f"{len(filtered_df)} transactions found for '{user_postcode}'")
+            st.dataframe(filtered_df.sort_values("date", ascending=False), hide_index=True)
+        else:
+            st.warning("No transactions found for the entered postcode.")
 
 if __name__ == "__main__":
     render()
